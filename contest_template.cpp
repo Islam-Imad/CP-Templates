6c91a9e8fbb59ba87da0d3d8c913002c87a4c650
islamimad <islamimad404@gmail.com>
2023-12-06 20:45:10 +0200

    added PURQ and contest_template

---
+++ contest_template.cpp
@@ -0,0 +1,27 @@
+/*
+    a template for competitive programming
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define X first
+#define Y second
+
+void solve(){
+    
+}
+
+int main()
+{
+ios_base::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+#endif
+int tc=1;
+// cin>>tc;
+    while(tc--){
+        solve();
+    }
+}
