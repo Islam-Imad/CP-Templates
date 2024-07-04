20ab4fc68ef80702dbb07e6fc22b7fe593082b17
pknowledge <islamimad404@gmail.com>
2024-07-04 10:01:26 +0300

    updated Cpp file

--- Cpp.cpp
+++ Cpp.cpp
@@ -5,21 +5,11 @@ using namespace std;
 #define X first
 #define Y second
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
+template <class T=int>
+using Tree = tree <T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
 
-template <typename K, typename V, typename Comp = std::less<K>>
-using ordered_map = tree<K, V, Comp, rb_tree_tag, tree_order_statistics_node_update>;
-
-template <typename K, typename Comp = std::less<K>>
-using ordered_set = ordered_map<K, null_type, Comp>;
-
-template <typename K, typename V, typename Comp = std::less_equal<K>>
-using ordered_multimap = tree<K, V, Comp, rb_tree_tag, tree_order_statistics_node_update>;
-
-template <typename K, typename Comp = std::less_equal<K>>
-using ordered_multiset = ordered_multimap<K, null_type, Comp>;
 void solve(){
     
 }
