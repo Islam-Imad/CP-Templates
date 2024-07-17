33bd3154e7385e54949a6901983c125d656b9614
Islam Imad <143586474+Islam-Imad@users.noreply.github.com>
2024-07-17 15:07:32 +0300

    Update Cpp.cpp

--- Cpp.cpp
+++ Cpp.cpp
@@ -10,6 +10,16 @@ using namespace __gnu_pbds;
 template <class T=int>
 using Tree = tree <T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
 
+ll modpow(ll base,ll exp , ll mod){
+    base%=mod;
+    ll res = 1;
+    while(exp){
+        if(exp&1)res = res*base%mod;
+        base = base*base%mod, exp>>=1;
+    }
+    return res;
+}
+
 void solve(){
     
 }
