2ff7018acf4504fd051d1a1196231cc22ae8f97f
Islam-Imad <islmaimad404@gmail.com>
2024-11-27 16:33:55 +0200

    added python template

---
+++ General/contest_template.py
@@ -0,0 +1,15 @@
+import math
+
+si = lambda : input()
+ii = lambda : int(input())
+mi = lambda : map(int,input().split())
+li = lambda : list(mi())
+
+def solve():
+    pass 
+def main():
+    t = 1
+    # t = ii()
+    for _ in range(t):
+        solve()
+main()
